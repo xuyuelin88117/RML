ac2f11962c92027d13e8b79e05807ce3d3fda0b5
Yuelin Xu <yuelin.xu@cispa.de>
2024-02-01 16:49:12 +0100

    renamed:    run_train_cifar.sh -> run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=train_cifar
+#SBATCH --job-name=RML-sh
 #SBATCH --partition=a100  # 或者您可以选择其他可用的 GPU 分区
 #SBATCH --gres=gpu:4  # 请求 4 个 GPU
 #SBATCH --time=8:00:00  # 或者您需要的时间
@@ -10,5 +10,7 @@
 
 # 运行您的 Python 脚本
 srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
-python ~/CISPA-home/Overfitting-in-adversarially-robust-deep-learning/train_cifar.py
-
+python ~/CISPA-home/RML/train_cifar.py
+python ~/CISPA-home/RML/train_cifar.py --attack fgsm
+python ~/CISPA-home/RML/train_cifar.py --cutout True --cutout-len 16 --mixup True --mixup-alpha 1
+python ~/CISPA-home/RML/train_cifar.py --attack fgsm --cutout True --cutout-len 16 --mixup True --mixup-alpha 1
