5c4f812a31f2f1163dd3abf325485fe3a1afd571
Yuelin Xu <yuelin.xu@cispa.de>
2024-02-02 22:52:44 +0100

    failed and modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=RML-sh
-#SBATCH --partition=a100  # 或者您可以选择其他可用的 GPU 分区
+#SBATCH --partition=gpu  # 或者您可以选择其他可用的 GPU 分区
 #SBATCH --gres=gpu:2  # 请求GPU个数
-#SBATCH --time=24:00:00  # 或者您需要的时间
 #SBATCH --output=job-%j.out
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=<yuelin.xu@cispa.de>
@@ -14,6 +13,6 @@ python ~/CISPA-home/RML/train_cifar.py
 srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
 python ~/CISPA-home/RML/train_cifar.py --attack fgsm
 srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
-python ~/CISPA-home/RML/train_cifar.py --cutout True --cutout-len 16 --mixup True --mixup-alpha 1
+python ~/CISPA-home/RML/train_cifar.py --cutout --cutout-len 16 --mixup --mixup-alpha 1
 srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
-python ~/CISPA-home/RML/train_cifar.py --attack fgsm --cutout True --cutout-len 16 --mixup True --mixup-alpha 1
+python ~/CISPA-home/RML/train_cifar.py --attack fgsm --cutout --cutout-len 16 --mixup --mixup-alpha 1
