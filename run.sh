d87fddd04893441a06dfaf1b8a45717dfbeb5d28
Yuelin Xu <yuelin.xu@cispa.de>
2024-02-02 11:09:45 +0100

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -11,6 +11,9 @@
 # 运行您的 Python 脚本
 srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
 python ~/CISPA-home/RML/train_cifar.py
+srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
 python ~/CISPA-home/RML/train_cifar.py --attack fgsm
+srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
 python ~/CISPA-home/RML/train_cifar.py --cutout True --cutout-len 16 --mixup True --mixup-alpha 1
+srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
 python ~/CISPA-home/RML/train_cifar.py --attack fgsm --cutout True --cutout-len 16 --mixup True --mixup-alpha 1
