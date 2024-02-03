3c5a64545270443f4e608651ffe8f9edf29edbdf
Yuelin Xu <yuelin.xu@cispa.de>
2024-02-04 00:52:08 +0100

    failed

--- run.sh
+++ run.sh
@@ -2,25 +2,19 @@
 
 #SBATCH --job-name=RML-model
 #SBATCH --partition=gpu  # 或者您可以选择其他可用的 GPU 分区
-#SBATCH --gres=gpu:4  # 请求GPU个数
+#SBATCH --gres=gpu:2  # 请求GPU个数
 #SBATCH --output=job-%j.out
-#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user=<yuelin.xu@cispa.de>
 
+#SBATCH --container-image pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime
+
 # 运行您的 Python 脚本
-srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
 python ~/CISPA-home/RML/train_cifar.py
-srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
 python ~/CISPA-home/RML/train_cifar.py --attack fgsm
-srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
 python ~/CISPA-home/RML/train_cifar.py --cutout --cutout-len 16 --mixup --mixup-alpha 1
-srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
 python ~/CISPA-home/RML/train_cifar.py --attack fgsm --cutout --cutout-len 16 --mixup --mixup-alpha 1
-srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
 python ~/CISPA-home/RML/train_cifar.py --model WideResNet
-srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
 python ~/CISPA-home/RML/train_cifar.py --model WideResNet --attack fgsm
-srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
 python ~/CISPA-home/RML/train_cifar.py --model WideResNet --cutout --cutout-len 16 --mixup --mixup-alpha 1
-srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
 python ~/CISPA-home/RML/train_cifar.py --model WideResNet --attack fgsm --cutout --cutout-len 16 --mixup --mixup-alpha 1
